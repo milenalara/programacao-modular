1fb170faeeb90c2e825ae485f6a5db4ce1c80a60
Milena Lara <milenalrf@gmail.com>
2023-08-17 20:39:07 -0300

    exercise 3 done

--- lista01/Aplicativo.java
+++ lista01/Aplicativo.java
@@ -1,36 +1,42 @@
 public class Aplicativo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // // EXERCICIO 1
-        int[] vet1 = new int[6];
-        int[] vetInvertido;
-        Exercicio01 exercicio1 = new Exercicio01();
+        // int[] vet1 = new int[6];
+        // int[] vetInvertido;
+        // Exercicio01 exercicio1 = new Exercicio01();
 
-        exercicio1.preencheVet(vet1);
-        exercicio1.imprimeVet(vet1);
+        // exercicio1.preencheVet(vet1);
+        // exercicio1.imprimeVet(vet1);
 
-        vetInvertido = exercicio1.inverteVet(vet1);
-        exercicio1.imprimeVet(vetInvertido);
+        // vetInvertido = exercicio1.inverteVet(vet1);
+        // exercicio1.imprimeVet(vetInvertido);
 
-        // EXERCICIO 2 - testando vetor com tamanho PAR
-        int[] vet2 = new int[6];
-        int[] vetSomaVizinhos;
-        Exercicio02 exercicio2 = new Exercicio02();
+        // // EXERCICIO 2 - testando vetor com tamanho PAR
+        // int[] vet2 = new int[6];
+        // int[] vetSomaVizinhos;
+        // Exercicio02 exercicio2 = new Exercicio02();
 
-        // preenchendo vetor original
-        exercicio2.preencheVet(vet2);
-        exercicio2.imprimeVet(vet2);
+        // // preenchendo vetor original
+        // exercicio2.preencheVet(vet2);
+        // exercicio2.imprimeVet(vet2);
 
-        vetSomaVizinhos = exercicio2.somaValoresVizinhos(vet2);
-        exercicio2.imprimeVet(vetSomaVizinhos);
+        // vetSomaVizinhos = exercicio2.somaValoresVizinhos(vet2);
+        // exercicio2.imprimeVet(vetSomaVizinhos);
 
-        // EXERCICIO 2 - testando vetor com tamanho IMPAR
-        int[] vet3 = new int[5];
+        // // EXERCICIO 2 - testando vetor com tamanho IMPAR
+        // int[] vet3 = new int[5];
 
-        // preenchendo vetor original
-        exercicio2.preencheVet(vet3);
-        exercicio2.imprimeVet(vet3);
+        // // preenchendo vetor original
+        // exercicio2.preencheVet(vet3);
+        // exercicio2.imprimeVet(vet3);
 
-        vetSomaVizinhos = exercicio2.somaValoresVizinhos(vet3);
-        exercicio2.imprimeVet(vetSomaVizinhos);
+        // vetSomaVizinhos = exercicio2.somaValoresVizinhos(vet3);
+        // exercicio2.imprimeVet(vetSomaVizinhos);
+
+        // EXERCICIO 3
+        Exercicio03 exercicio3 = new Exercicio03();
+
+        exercicio3.obterMedidas();
+        exercicio3.imprimeRetangulo();
     }
 }
--- lista01/Exercicio03.java
+++ lista01/Exercicio03.java
@@ -5,8 +5,14 @@
 // X   X
 // XXXXX
 
+import java.util.Scanner;
+
 public class Exercicio03 {
     private int altura, largura;
+    private String primeiraUltimaLinha = "";
+    private String linhasInterm = "";
+    private String retangulo = "";
+    Scanner sc = new Scanner(System.in);
 
     public int getAltura() {
         return altura;
@@ -30,5 +36,39 @@ public class Exercicio03 {
     Exercicio03(int altura, int largura) {
         this.altura = altura;
         this.largura = largura;
+    }
+
+    public void obterMedidas() {
+        System.out.println("Informe a altura do retangulo: ");
+        this.altura = sc.nextInt();
+        System.out.println("Informe a largura do retangulo: ");
+        this.largura = sc.nextInt();
+    }
+
+    private void constroiRetangulo() throws Exception {
+        if (this.altura < 2 || this.largura < 2) {
+            throw new Exception("Não é possível produzir um retângulo com dimensões menores que 2x2");
+        } else {
+            for (int i = 0; i < largura; i++) {
+                primeiraUltimaLinha += "X";
+            }
+
+            linhasInterm += "X";
+            for (int i = 0; i < largura - 2; i++) {
+                linhasInterm += " ";
+            }
+            linhasInterm += "X";
+
+            retangulo += primeiraUltimaLinha + "\n";
+            for (int i = 0; i < altura - 2; i++) {
+                retangulo += linhasInterm + "\n";
+            }
+            retangulo += primeiraUltimaLinha;
+        }
+    }
+
+    public void imprimeRetangulo() throws Exception {
+        constroiRetangulo();
+        System.out.println(retangulo);
     }
 }
