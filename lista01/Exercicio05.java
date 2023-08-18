598641b0e2514bcb4461f9530c96a826f34660de
Milena Lara <milenalrf@gmail.com>
2023-08-17 22:04:58 -0300

    exercícios teórios de 13 a 16

---
+++ lista01/Exercicio05.java
@@ -0,0 +1,5 @@
+// 05 – Um estudante muito organizado está matriculado em N disciplinas, e dispõe de um total semanal de H horas e M minutos para estudar. Sua intenção é dividir o tempo disponível igualmente para as N disciplinas, e distribuir o tempo de descanso que sobrar nas pausas entre as disciplinas. Crie um ou mais métodos para informar ao estudante como ele deve organizar seu plano de estudos.
+
+public class Exercicio05 {
+
+}
